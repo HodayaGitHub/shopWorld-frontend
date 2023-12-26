5f9a9d1a1a1970f50e17565961ea167420147767
HodayaGitHub <hodaya1812@gmail.com>
2023-12-26 18:58:03 +0200

    change titles for chart

--- src/cmps/ChartPie.jsx
+++ src/cmps/ChartPie.jsx
@@ -4,14 +4,14 @@ import { Pie } from "react-chartjs-2";
 export function ChartPie({ chartData }) {
     return (
         <div className="chart-container">
-            <h2 className="chart-txt" style={{ textAlign: "center" }}>Pie Chart</h2>
+            <h2 className="chart-txt" style={{ textAlign: "center" }}>Toys stock status</h2>
             <Pie
                 data={chartData}
                 options={{
                     plugins: {
                         title: {
                             display: true,
-                            text: "Toys stock status"
+                            text: "In stock \ out of stock"
                         }
                     }
                 }}
